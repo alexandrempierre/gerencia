da052cac5ce8919780634df371802ef8b8dcf0b6
Fausto Junqueira <faustoferjunqueira@gmail.com>
2016-04-02 02:25:40 -0300

    Simple Running

---
+++ src/main/java/br/ufrj/dcc/gerencia/application/Application.java
@@ -0,0 +1,16 @@
+package br.ufrj.dcc.gerencia.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by fausto on 4/2/16.
+ */
+
+@SpringBootApplication
+public class Application {
+  public static void main(String[] args){
+    SpringApplication.run(Application.class, args);
+  }
+}
+
