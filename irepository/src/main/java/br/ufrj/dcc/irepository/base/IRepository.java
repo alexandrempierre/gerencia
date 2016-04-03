d758872b2d2bbbce7fbd7ecf3add5ba94a530b97
Fausto Junqueira <faustoferjunqueira@gmail.com>
2016-04-03 12:23:58 -0300

    Integrate Irepository with repository

--- irepository/src/main/java/br/ufrj/dcc/irepository/base/IRepository.java
+++ irepository/src/main/java/br/ufrj/dcc/irepository/base/IRepository.java
@@ -3,5 +3,5 @@ package br.ufrj.dcc.irepository.base;
 /**
  * Created by fausto on 4/3/16.
  */
-public class IRepository {
+public interface IRepository {
 }
--- repository/src/main/java/br/ufrj/dcc/gerencia/repository/base/Repository.java
+++ repository/src/main/java/br/ufrj/dcc/gerencia/repository/base/Repository.java
@@ -1,7 +1,9 @@
 package br.ufrj.dcc.gerencia.repository.base;
 
+import br.ufrj.dcc.irepository.base.IRepository;
+
 /**
  * Created by fausto on 4/3/16.
  */
-public class Repository {
+public class Repository implements IRepository{
 }
