a303ebd77fc508bcb06080b8452e0193e062173b
Fausto Junqueira <faustoferjunqueira@gmail.com>
2016-04-03 12:27:40 -0300

    Create domain

---
+++ domain/src/main/java/br/ufrj/dcc/gerencia/domain/base/LCIModel.java
@@ -0,0 +1,17 @@
+package br.ufrj.dcc.gerencia.domain.base;
+
+/**
+ * Created by fausto on 4/3/16.
+ */
+public class LCIModel {
+
+  private int id;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+}
