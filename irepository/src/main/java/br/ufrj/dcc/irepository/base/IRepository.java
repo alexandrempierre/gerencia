6d7416df7f4c8996adde95409d6ee584019fa349
Fausto Junqueira <faustoferjunqueira@gmail.com>
2016-04-03 22:06:48 -0300

    Create methods save and get

--- irepository/src/main/java/br/ufrj/dcc/irepository/base/IRepository.java
+++ irepository/src/main/java/br/ufrj/dcc/irepository/base/IRepository.java
@@ -1,7 +1,12 @@
 package br.ufrj.dcc.irepository.base;
 
+import br.ufrj.dcc.gerencia.domain.base.LCIModel;
+
 /**
  * Created by fausto on 4/3/16.
  */
-public interface IRepository {
+public interface IRepository<M extends LCIModel>{
+  int save(M register);
+  //query(specification)
+  M get(int id);
 }
