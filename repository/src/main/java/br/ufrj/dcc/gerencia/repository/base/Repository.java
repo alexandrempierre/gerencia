e93a53ea903fb42371ed1ca6fe49b165e04295fd
Fausto Junqueira <faustoferjunqueira@gmail.com>
2016-04-03 23:08:31 -0300

    Implement poc of Repository

--- repository/src/main/java/br/ufrj/dcc/gerencia/repository/base/Repository.java
+++ repository/src/main/java/br/ufrj/dcc/gerencia/repository/base/Repository.java
@@ -1,9 +1,18 @@
 package br.ufrj.dcc.gerencia.repository.base;
 
+import br.ufrj.dcc.gerencia.domain.base.LCIModel;
 import br.ufrj.dcc.irepository.base.IRepository;
 
 /**
  * Created by fausto on 4/3/16.
  */
-public class Repository implements IRepository{
+public class Repository<M extends LCIModel> implements IRepository<M>{
+
+  public int save(M register) {
+    return 0;
+  }
+
+  public M get(int id) {
+    return null;
+  }
 }
