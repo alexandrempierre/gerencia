2b2d0d353db91985d7f2128e80a3ed78b52937d3
Fausto Junqueira <faustoferjunqueira@gmail.com>
2016-04-03 23:19:04 -0300

    Implement CrudFacade

--- business/src/main/java/br/ufrj/dcc/gerencia/business/base/CrudFacade.java
+++ business/src/main/java/br/ufrj/dcc/gerencia/business/base/CrudFacade.java
@@ -1,7 +1,33 @@
 package br.ufrj.dcc.gerencia.business.base;
 
+import br.ufrj.dcc.gerencia.domain.base.LCIModel;
+import br.ufrj.dcc.irepository.base.IRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 /**
  * Created by fausto on 4/3/16.
  */
-public class CrudFacade {
+public class CrudFacade
+  <M extends LCIModel, R extends IRepository<M>>
+{
+
+  private R repository;
+
+  protected R getRepository() {
+    return repository;
+  }
+
+  @Autowired
+  public CrudFacade(R repository){
+    this.repository = repository;
+  }
+
+  int save(M register){
+    return getRepository().save(register);
+  }
+
+  M get(int id){
+    return getRepository().get(id);
+  }
+
 }
