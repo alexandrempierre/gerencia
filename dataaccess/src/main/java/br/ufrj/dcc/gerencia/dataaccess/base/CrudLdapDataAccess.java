b1c97d0af488807778f383eeb1cfcee35823e540
Fausto Junqueira <faustoferjunqueira@gmail.com>
2016-04-05 14:36:13 -0300

    Create dataAccess module

---
+++ dataaccess/src/main/java/br/ufrj/dcc/gerencia/dataaccess/base/CrudLdapDataAccess.java
@@ -0,0 +1,11 @@
+package br.ufrj.dcc.gerencia.dataaccess.base;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by fausto on 4/5/16.
+ */
+
+@Component
+public class CrudLdapDataAccess{
+}
---
+++ dataaccess/src/main/java/br/ufrj/dcc/gerencia/dataaccess/entities/ExampleLDAPDataAccess.java
@@ -0,0 +1,13 @@
+package br.ufrj.dcc.gerencia.dataaccess.entities;
+
+import br.ufrj.dcc.gerencia.dataaccess.base.CrudLdapDataAccess;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by fausto on 4/5/16.
+ */
+
+@Component
+public class ExampleLDAPDataAccess extends CrudLdapDataAccess{
+
+}
