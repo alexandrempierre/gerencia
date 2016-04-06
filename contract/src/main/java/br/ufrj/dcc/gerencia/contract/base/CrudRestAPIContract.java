fb14ba4de762b8a426a00fc9c626fa2e1d8c0650
Fausto Junqueira <faustoferjunqueira@gmail.com>
2016-04-06 08:17:56 -0300

    Integrate Service With Contract and business

--- contract/src/main/java/br/ufrj/dcc/gerencia/contract/base/CrudRestAPIContract.java
+++ contract/src/main/java/br/ufrj/dcc/gerencia/contract/base/CrudRestAPIContract.java
@@ -1,20 +1,28 @@
 package br.ufrj.dcc.gerencia.contract.base;
 
 import br.ufrj.dcc.gerencia.domain.base.LCIModel;
+import br.ufrj.dcc.gerencia.domain.base.LciSpecification;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 /**
  * Created by fausto on 4/3/16.
  */
 
-public interface CrudRestAPIContract<M extends LCIModel> {
+public interface CrudRestAPIContract<M extends LCIModel, S extends LciSpecification> {
 
-  @RequestMapping(value = "/save", method = RequestMethod.POST)
-  int save(M register);
+  @RequestMapping(value="/save", method=RequestMethod.POST)
+  M save(M register);
 
-  @RequestMapping(value="/get/{id}", method = RequestMethod.GET)
-  M get(@PathVariable("id") int id);
+  @RequestMapping(value="/list", method=RequestMethod.GET)
+  List<M> query(S specification);
 
+  @RequestMapping(value="/get", method=RequestMethod.GET)
+  M get(String uid);
+
+  @RequestMapping(value="/delete", method=RequestMethod.DELETE)
+  M delete(String uid);
 }
--- service/src/main/java/br/ufrj/dcc/gerencia/service/base/CrudRestAPI.java
+++ service/src/main/java/br/ufrj/dcc/gerencia/service/base/CrudRestAPI.java
@@ -3,10 +3,14 @@ package br.ufrj.dcc.gerencia.service.base;
 import br.ufrj.dcc.gerencia.business.base.CrudFacade;
 import br.ufrj.dcc.gerencia.contract.base.CrudRestAPIContract;
 import br.ufrj.dcc.gerencia.domain.base.LCIModel;
+import br.ufrj.dcc.gerencia.domain.base.LciSpecification;
 import br.ufrj.dcc.gerencia.repository.base.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 /**
  * Created by fausto on 4/3/16.
@@ -15,9 +19,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Component
 public abstract class CrudRestAPI
   <M extends LCIModel,
-   B extends CrudFacade<M,R>,
-   R extends Repository<M>>
-  implements CrudRestAPIContract<M>{
+   S extends LciSpecification,
+   B extends CrudFacade<M,S,R>,
+   R extends Repository<M,S>>
+  implements CrudRestAPIContract<M,S>{
 
   @Autowired
   private B facade;
@@ -26,11 +31,16 @@ public abstract class CrudRestAPI
     return facade;
   }
 
-  public int save(M register) {
+  public M save(M register){
     return getFacade().save(register);
   }
-
-  public M get(@PathVariable("id") int id) {
-    return getFacade().get(id);
+  public List<M> query(S specification){
+    return getFacade().query(specification);
+  }
+  public M get(@RequestParam("uid") String uid){
+    return getFacade().get(uid);
+  }
+  public M delete(@RequestParam("uid") String uid){
+    return getFacade().delete(uid);
   }
 }
