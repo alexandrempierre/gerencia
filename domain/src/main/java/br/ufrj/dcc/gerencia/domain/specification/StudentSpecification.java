e890b389c040f7187416d6f3de30023fcdbb8dad
Fausto Junqueira <faustoferjunqueira@gmail.com>
2016-04-13 09:31:53 -0300

    Filter for student ready, however there are TODOs

--- domain/src/main/java/br/ufrj/dcc/gerencia/domain/specification/StudentSpecification.java
+++ domain/src/main/java/br/ufrj/dcc/gerencia/domain/specification/StudentSpecification.java
@@ -1,7 +1,10 @@
 package br.ufrj.dcc.gerencia.domain.specification;
 
 import br.ufrj.dcc.gerencia.domain.base.LciLdapSpecification;
+import org.springframework.ldap.filter.AndFilter;
+import org.springframework.ldap.filter.LikeFilter;
 import org.springframework.ldap.query.LdapQuery;
+import org.springframework.ldap.query.LdapQueryBuilder;
 
 import javax.naming.Name;
 
@@ -12,6 +15,28 @@ import static org.springframework.ldap.query.LdapQueryBuilder.query;
  */
 public class StudentSpecification extends LciLdapSpecification{
   private String uid;
+  private String name;
+  private String surname;
+  private String dre;
+  private Long from;
+  private Long to;
+  private Boolean expired; //TODO: add this filter
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getSurname() {
+    return surname;
+  }
+
+  public void setSurname(String surname) {
+    this.surname = surname;
+  }
 
   public String getUid() {
     return uid;
@@ -21,8 +46,74 @@ public class StudentSpecification extends LciLdapSpecification{
     this.uid = uid;
   }
 
+  public String getDre() {
+    return dre;
+  }
+
+  public void setDre(String dre) {
+    this.dre = dre;
+  }
+
+  public Long getFrom() {
+    return from;
+  }
+
+  public void setFrom(Long from) {
+    this.from = from;
+  }
+
+  public Long getTo() {
+    return to;
+  }
+
+  public void setTo(Long to) {
+    this.to = to;
+  }
+
+  public Boolean getExpired() {
+    return expired;
+  }
+
+  public void setExpired(Boolean expired) {
+    this.expired = expired;
+  }
+
   @Override
   public LdapQuery toQuery(Name base) {
-    return query().base(base).where("uid").is(uid);
+    LdapQueryBuilder queryBuilder = query().base(base);
+    AndFilter andFilter = new AndFilter();
+
+    //Para garantir que todos os usuario apareçam
+    andFilter.and(new LikeFilter("uid", "*"));
+
+    if(uid != null && uid.length() > 0){
+      System.out.println("uid("+uid.length()+"): "+uid);
+      andFilter.and(new LikeFilter("uid", "*"+uid+"*"));
+    }
+
+    if(name != null && name.length() > 0){
+      System.out.println("cn("+name.length()+"): "+name);
+      andFilter.and(new LikeFilter("cn", "*"+name+"*"));
+    }
+
+    if(surname != null && surname.length() > 0){
+      System.out.println("sn("+surname.length()+"): "+surname);
+      andFilter.and(new LikeFilter("sn", "*"+surname+"*"));
+    }
+
+    if(dre != null && dre.length() > 0){
+      System.out.println("dre("+dre.length()+"): "+dre);
+      andFilter.and(new LikeFilter("dre", "*"+dre+"*"));
+    }
+
+    if(to != null && to > 0){
+      //TODO: fazer o filtro aqui
+    }
+
+    if(from != null && from > 0){
+      //TODO: fazer o filtro aqui
+    }
+
+    return queryBuilder.filter(andFilter);
   }
 }
