af933a27b889c514cfeac163106fa15e9568d1b4
Fausto Junqueira <faustoferjunqueira@gmail.com>
2016-04-16 11:55:30 -0300

    Student Edit without alter password

--- web/src/main/websrc/src/controller/aluno/alunoDetalheCtrl.js
+++ web/src/main/websrc/src/controller/aluno/alunoDetalheCtrl.js
@@ -3,27 +3,15 @@
  */
 
 (function(app){
-  app.controller('alunoDetalheCtrl', function($scope,lciCrudScope, alunoAPI,caminho,registroTO, alterarSenhaModal, validacaoAPI){
+  app.controller('alunoDetalheCtrl', function($scope,lciCrudScope, alunoAPI,caminho,registroTO, /*alterarSenhaModal,*/ validacaoAPI){
     lciCrudScope.Scope.call($scope,alunoAPI,caminho);
-
-    $scope.abrirAlterarSenha = alterarSenhaModal.abrirModal;
-
-    //Sobrecarregando o método salvar
-    var salvarFn = $scope.salvar;
-    $scope.salvar = function(registro){
-      if(registro.usuario.infinito){
-        registro.usuario.expiracao = null;
-      }
-      salvarFn(registro);
-    };
-
+    
+    /*$scope.abrirAlterarSenha = alterarSenhaModal.abrirModal;*/
+    
     // Para validar o login
-    $scope.loginUnico = validacaoAPI.loginUnico;
-    // Para validar o CPF
-    $scope.cpfUnico = validacaoAPI.cpfUnico;
+    $scope.loginUnico = validacaoAPI.loginUnico(alunoAPI);
 
     $scope.aluno = registroTO.data;
-    $scope.aluno.usuario.infinito = !$scope.aluno.usuario.expiracao;
 
   });
 
