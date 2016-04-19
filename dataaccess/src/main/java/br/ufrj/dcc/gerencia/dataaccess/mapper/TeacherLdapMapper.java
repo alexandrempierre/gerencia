8009dce353855c1e8a1755803739495fcc1f11d5
Fausto Junqueira <faustoferjunqueira@gmail.com>
2016-04-19 09:51:30 -0300

    Comment properties

--- dataaccess/src/main/java/br/ufrj/dcc/gerencia/dataaccess/mapper/TeacherLdapMapper.java
+++ dataaccess/src/main/java/br/ufrj/dcc/gerencia/dataaccess/mapper/TeacherLdapMapper.java
@@ -1,7 +1,6 @@
 package br.ufrj.dcc.gerencia.dataaccess.mapper;
 
 import br.ufrj.dcc.gerencia.dataaccess.base.LCIUserAbstractContextMapper;
-import br.ufrj.dcc.gerencia.domain.entities.Example;
 import br.ufrj.dcc.gerencia.domain.entities.Teacher;
 import org.springframework.ldap.core.DirContextOperations;
 import static br.ufrj.dcc.gerencia.domain.auxiliar.ObjectClassLdap.*;
@@ -15,40 +14,36 @@ import org.springframework.stereotype.Component;
 public class TeacherLdapMapper extends LCIUserAbstractContextMapper<Teacher>{
   @Override
   public void mapperResult(Teacher teacher, DirContextOperations ctx) {
-    try {
-      teacher.getUser().setLimitHDSpace(new Integer(ctx.getStringAttribute("cota")));
-      teacher.getUser().setLogin(ctx.getStringAttribute("uid"));
-      teacher.getPerson().setName(ctx.getStringAttribute("cn"));
-      teacher.getPerson().setSurname(ctx.getStringAttribute("sn"));
-      teacher.getPerson().setExternMail(ctx.getStringAttribute("emailExterno"));
-      teacher.getPerson().setPhoneNumber(ctx.getStringAttribute("telephoneNumber"));
-      teacher.getPerson().setDisplayName(ctx.getStringAttribute("displayName"));
-      teacher.getServerData().setHomeDirectory(ctx.getStringAttribute("homeDirectory"));
-      teacher.getServerData().setLoginShellApplication(ctx.getStringAttribute("loginShell"));
-      teacher.getServerData().setGidNumber(new Integer(ctx.getStringAttribute("gidNumber")));
-      teacher.getServerData().setUidNumber(new Integer(ctx.getStringAttribute("uidNumber")));
-      teacher.getServerData().setGecos(ctx.getStringAttribute("gecos"));
-      teacher.getSamba().setSambaSID(ctx.getStringAttribute("sambaSID"));
-      teacher.getSamba().setSambaAcctFlags(ctx.getStringAttribute("sambaAcctFlags"));
-      teacher.getSamba().setSambaKickoffTime(new Long(ctx.getStringAttribute("sambaKickoffTime")));
-      teacher.getSamba().setSambaLMPassword(ctx.getStringAttribute("sambaLMPassword"));
-      teacher.getSamba().setSambaNTPassword(ctx.getStringAttribute("sambaNTPassword"));
-      teacher.getSamba().setSambaPrimaryGroupSID(ctx.getStringAttribute("sambaPrimaryGroupSID"));
-      teacher.getSamba().setSambaPwdCanChange(new Long(ctx.getStringAttribute("sambaPwdCanChange")));
-      teacher.getSamba().setSambaPwdLastSet(new Long(ctx.getStringAttribute("sambaPwdLastSet")));
-      teacher.getSamba().setSambaPwdMustChange(new Long(ctx.getStringAttribute("sambaPwdMustChange")));
-      teacher.getShadow().setShadowExpire(new Long(ctx.getStringAttribute("shadowExpire")));
-      teacher.getShadow().setShadowFlag(new Integer(ctx.getStringAttribute("shadowFlag")));
-      teacher.getShadow().setShadowInactive(new Integer(ctx.getStringAttribute("shadowInactive")));
-      teacher.getShadow().setShadowLastChange(new Long(ctx.getStringAttribute("shadowLastChange")));
-      teacher.getShadow().setShadowMax(new Integer(ctx.getStringAttribute("shadowMax")));
-      teacher.getShadow().setShadowMin(new Integer(ctx.getStringAttribute("shadowMin")));
-      teacher.getShadow().setShadowWarning(new Integer(ctx.getStringAttribute("shadowWarning")));
-      teacher.setId(teacher.getUser().getLogin());
-      teacher.setOperator(ctx.getStringAttribute("monitor").equals("1"));
-    }catch (NullPointerException e){
-      e.printStackTrace();
-    }
+    teacher.getUser().setLimitHDSpace(new Integer(ctx.getStringAttribute("cota")));
+    teacher.getUser().setLogin(ctx.getStringAttribute("uid"));
+    teacher.getPerson().setName(ctx.getStringAttribute("cn"));
+    teacher.getPerson().setSurname(ctx.getStringAttribute("sn"));
+    teacher.getPerson().setExternMail(ctx.getStringAttribute("emailExterno"));
+    teacher.getPerson().setPhoneNumber(ctx.getStringAttribute("telephoneNumber"));
+    teacher.getPerson().setDisplayName(ctx.getStringAttribute("displayName"));
+    teacher.getServerData().setHomeDirectory(ctx.getStringAttribute("homeDirectory"));
+    teacher.getServerData().setLoginShellApplication(ctx.getStringAttribute("loginShell"));
+    teacher.getServerData().setGidNumber(new Integer(ctx.getStringAttribute("gidNumber")));
+    teacher.getServerData().setUidNumber(new Integer(ctx.getStringAttribute("uidNumber")));
+    teacher.getServerData().setGecos(ctx.getStringAttribute("gecos"));
+    teacher.getSamba().setSambaSID(ctx.getStringAttribute("sambaSID"));
+    teacher.getSamba().setSambaAcctFlags(ctx.getStringAttribute("sambaAcctFlags"));
+    teacher.getSamba().setSambaKickoffTime(new Long(ctx.getStringAttribute("sambaKickoffTime")));
+    teacher.getSamba().setSambaLMPassword(ctx.getStringAttribute("sambaLMPassword"));
+    teacher.getSamba().setSambaNTPassword(ctx.getStringAttribute("sambaNTPassword"));
+    teacher.getSamba().setSambaPrimaryGroupSID(ctx.getStringAttribute("sambaPrimaryGroupSID"));
+    teacher.getSamba().setSambaPwdCanChange(new Long(ctx.getStringAttribute("sambaPwdCanChange")));
+    teacher.getSamba().setSambaPwdLastSet(new Long(ctx.getStringAttribute("sambaPwdLastSet")));
+    teacher.getSamba().setSambaPwdMustChange(new Long(ctx.getStringAttribute("sambaPwdMustChange")));
+    teacher.getShadow().setShadowExpire(new Long(ctx.getStringAttribute("shadowExpire")));
+    teacher.getShadow().setShadowFlag(new Integer(ctx.getStringAttribute("shadowFlag")));
+    teacher.getShadow().setShadowInactive(new Integer(ctx.getStringAttribute("shadowInactive")));
+    teacher.getShadow().setShadowLastChange(new Long(ctx.getStringAttribute("shadowLastChange")));
+    teacher.getShadow().setShadowMax(new Integer(ctx.getStringAttribute("shadowMax")));
+    teacher.getShadow().setShadowMin(new Integer(ctx.getStringAttribute("shadowMin")));
+    teacher.getShadow().setShadowWarning(new Integer(ctx.getStringAttribute("shadowWarning")));
+    teacher.setId(teacher.getUser().getLogin());
+    //teacher.setOperator(ctx.getStringAttribute("monitor").equals("1"));
   }
 
   @Override
@@ -56,7 +51,7 @@ public class TeacherLdapMapper extends LCIUserAbstractContextMapper<Teacher>{
     ctx.setAttributeValues("objectclass", TEACHER_OBJECT_CLASS);
 
     //TODO: fazer departamento
-    ctx.setAttributeValue("departamento", "nothing");
+    //ctx.setAttributeValue("departamento", "nothing");
 
     ctx.setAttributeValue("cota", Integer.toString(register.getUser().getLimitHDSpace()));
     ctx.setAttributeValue("uid", register.getUser().getLogin());
@@ -87,6 +82,6 @@ public class TeacherLdapMapper extends LCIUserAbstractContextMapper<Teacher>{
     ctx.setAttributeValue("shadowMin", Integer.toString(register.getShadow().getShadowMin()));
     ctx.setAttributeValue("shadowWarning", Integer.toString(register.getShadow().getShadowWarning()));
     ctx.setAttributeValue("userPassword", register.getUser().getPassword());
-    ctx.setAttributeValue("monitor", register.isOperator() ? "1" : "0");
+    //ctx.setAttributeValue("monitor", register.isOperator() ? "1" : "0");
   }
 }
