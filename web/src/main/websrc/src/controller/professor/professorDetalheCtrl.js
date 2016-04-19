b58e6c2bc8734450f1ef9cb80b3d925f24c14d75
Fausto Junqueira <faustoferjunqueira@gmail.com>
2016-04-19 14:33:06 -0300

    +Teacher Detail

--- web/src/main/websrc/src/controller/professor/professorDetalheCtrl.js
+++ web/src/main/websrc/src/controller/professor/professorDetalheCtrl.js
@@ -3,28 +3,12 @@
  */
 
 (function(app){
-  app.controller('professorDetalheCtrl', function($scope,lciCrudScope, professorAPI,caminho,registroTO, alterarSenhaModal, validacaoAPI){
-    lciCrudScope.Scope.call($scope,professorAPI,caminho);
+  app.controller('professorDetalheCtrl', function($scope,lciCrudScope, teacherAPI,caminho,registroTO, changePasswordModal){
+    lciCrudScope.Scope.call($scope,teacherAPI,caminho);
 
-    $scope.abrirAlterarSenha = alterarSenhaModal.abrirModal;
+    $scope.changePasswordModalOpen = changePasswordModal.openModal;
 
-    //Sobrecarregando o método salvar
-    var salvarFn = $scope.salvar;
-    $scope.salvar = function(registro){
-      if(registro.usuario.infinito){
-        registro.usuario.expiracao = null;
-      }
-      salvarFn(registro);
-    };
-
-    // Para validar o login
-    $scope.loginUnico = validacaoAPI.loginUnico;
-    // Para validar o CPF
-    $scope.cpfUnico = validacaoAPI.cpfUnico;
-
-    $scope.professor = registroTO.data;
-    $scope.professor.usuario.infinito = !$scope.professor.usuario.expiracao;
-
+    $scope.teacher = registroTO.data;
   });
 
 })(angular.module('gerencia'));
